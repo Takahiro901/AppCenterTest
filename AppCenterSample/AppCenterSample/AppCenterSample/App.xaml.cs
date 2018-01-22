f6b055b6867d10a8b2df7b2258ce268d71693d62
貴浩 古川 <furu-0425.kawa@hotmail.co.jp>
2018-01-22 19:14:49 +0900

    プロジェクト追加

---
+++ AppCenterSample/AppCenterSample/AppCenterSample/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AppCenterSample
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new AppCenterSample.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            AppCenter.Start("3843f52d-a806-436d-b45a-7d6af4370df9",
+                   typeof(Analytics));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ AppCenterSample/AppCenterSample/AppCenterSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AppCenter.Analytics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCenterSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            Analytics.TrackEvent("mainpage");
+        }
+	}
+}
